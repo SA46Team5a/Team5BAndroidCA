a6c27a87b07058ee7a9ec133ff0a5265366c186b
Anthony <luo18chao@gmail.com>
2018-06-22 15:35:58 +0800

    added bookModel,BookDetailActivity and strings.xml

--- app/src/main/java/com/example/anthony/androidca/BookDetailActivity.java
+++ app/src/main/java/com/example/anthony/androidca/BookDetailActivity.java
@@ -1,7 +1,9 @@
 package com.example.anthony.androidca;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class BookDetailActivity extends Activity {
 
@@ -9,5 +11,19 @@ public class BookDetailActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_book_detail);
+        Intent i=getIntent();
+        String ISBN=i.getStringExtra("ISBN");
+        BookModel book=book.getbook(ISBN);
+        show(book);
+        void show(BookModel book)
+        {
+            int[] ids={R.id.editText1,R.id.editText2,R.id.editText3,R.id.editText4,R.id.editText5,R.id.editText6,R.id.editText7,R.id.editText8};
+            String[] keys={"ISBN","title", "authorName","categoryName","price","discountedPrice","stockLevel","synopsis"};
+            for(int i=0;i<keys.length;i++)
+            {
+                EditText e=(EditText) findViewById(ids[i]);
+                e.setText(book.get(keys[i]));
+            }
+        }
     }
 }
--- app/src/main/java/com/example/anthony/androidca/BookModel.java
+++ app/src/main/java/com/example/anthony/androidca/BookModel.java
@@ -1,4 +1,45 @@
 package com.example.anthony.androidca;
 
-public class BookModel {
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class BookModel extends HashMap<String,Object> {
+
+    final static String baseURL = "http://172.27.240.226:8090/workhere/Service.svc/";
+
+    public Employee(String name, String id, String address, String salary) {
+        put("Name", name);
+        put("Id", id);
+        put("Salary", salary);
+        put("Address", address);
+    }
+
+    public static List<String> list() {
+        List<String> list = new ArrayList<String>();
+        JSONArray a = JSONParser.getJSONArrayFromUrl(baseURL + "Employee");
+        try {
+            for (int i =0; i<a.length(); i++)
+                list.add(a.getString(i));
+        } catch (Exception e) {
+            Log.e("Employee.list()", "JSONArray error");
+        }
+        return(list);
+    }
+
+    public static Employee getEmp(String eid) {
+        JSONObject b = JSONParser.getJSONFromUrl(baseURL + "employee/" + eid);
+        try {
+            return new Employee(b.getString("Name"), b.getString("Id"),
+                    b.getString("Address"), b.getString("Salary"));
+        } catch (Exception e) {
+            Log.e("Employee.getEmp()", "JSONArray error");
+        }
+        return(null);
+    }
 }
